0443a6150ff21f774400556af7b746a4988e79bc
Aldhan (He/Him) <aldhanekadev@gmail.com>
2021-06-05 12:37:14 +0700

    add embed

--- utils/gtag.ts
+++ utils/gtag.ts
@@ -3,9 +3,9 @@ export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_TOKEN;
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url: URL) => {
   // @ts-ignore
-  window.gtag('config', GA_TRACKING_ID, {
-    page_path: url,
-  });
+  // window.gtag('config', GA_TRACKING_ID, {
+  //   page_path: url,
+  // });
 };
 
 type GTagEvent = {
@@ -18,9 +18,9 @@ type GTagEvent = {
 // https://developers.google.com/analytics/devguides/collection/gtagjs/events
 export const event = ({ action, category, label, value }: GTagEvent) => {
   // @ts-ignore
-  window.gtag('event', action, {
-    event_category: category,
-    event_label: label,
-    value: value,
-  });
+  // window.gtag('event', action, {
+  //   event_category: category,
+  //   event_label: label,
+  //   value: value,
+  // });
 };
