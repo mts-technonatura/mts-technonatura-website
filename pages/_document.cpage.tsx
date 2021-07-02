04cc1287b566908795031da54b78fd5dbdf01cd1
Aldhan (He/Him) <aldhanekadev@gmail.com>
2021-07-02 09:30:41 +0700

    Update _document.cpage.tsx

--- pages/_document.cpage.tsx
+++ pages/_document.cpage.tsx
@@ -127,6 +127,14 @@ export default class Root extends Document {
           <Main />
 
           <NextScript />
+          
+          
+        <script>
+            // before React is loaded
+            if (typeof window.__REACT_DEVTOOLS_GLOBAL_HOOK__ === 'object') {
+                __REACT_DEVTOOLS_GLOBAL_HOOK__.inject = function() {};
+            }
+        </script>
         </body>
       </Html>
     );
